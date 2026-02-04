75ddd5979c9e14e390dfa0ce285e66d786ef37b4
Lee Spitzley <lspitzley@users.noreply.github.com>
2026-02-04 17:58:06 +0000

    accessed arguments and stored them into variables

--- class_vars_args.py
+++ class_vars_args.py
@@ -5,10 +5,28 @@ and data types. It will also
 show how to read command-line 
 arguments.
 """
+# import external libraries
+import sys
+
+# view arguments with sys
+print(f'The arguments are {sys.argv}')
+
+# show indexing
+idx = 2
+# debug statement to show idx and the number of args
+print(f'idx = {idx}; sys.argv length = {len(sys.argv)}')
+
+# show a specific argument
+print(f'Element {idx} is {sys.argv[idx]}')
+# print('Element', idx, 'is', sys.argv[idx]) # uglier but equivalent
+
 
 # make a few variables
-x = 12
+x = int(sys.argv[1]) # int() forces the sting to an integer
 y = 25
+
+# show the type of the variables
+print(f'x is type {type(x)}; y is type {type(y)}')
 
 # print(x, y) # minimal information!
 print(f'x = {x}, y = {y}')
