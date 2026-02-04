6b2a03fbefa59a07768793664f6c27092301e50d
Lee Spitzley <lspitzley@users.noreply.github.com>
2026-02-04 17:18:03 +0000

    simple addition with two variables

---
+++ class_vars_args.py
@@ -0,0 +1,19 @@
+"""
+This script is going to show 
+different types of variables 
+and data types. It will also
+show how to read command-line 
+arguments.
+"""
+
+# make a few variables
+x = 12
+y = 25
+
+# print(x, y) # minimal information!
+print(f'x = {x}, y = {y}')
+
+# do some addition
+z = x + y
+
+print(f'z = {z}')
