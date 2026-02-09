1c25212ea4e3658622e3c0939c7d5ae013bf04bc
avamitchell12 <agmitchell@albany.edu>
2026-02-09 20:44:07 +0000

    lab for argv_lab

---
+++ 3-2_lab.py
@@ -0,0 +1,9 @@
+import sys
+print(f'The arguments are {sys.argv}') 
+idx = 2
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+print(f'x ={x}, y = {y}') 
+product = x * y
+
+print(f'product = {product}')
